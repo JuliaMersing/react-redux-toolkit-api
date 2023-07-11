8e6393362285cc872582a71dfb8768c0f2461eb7
Julia Mersing Ortiz <mariajulia.mersing@clearpay.com>
2023-07-11 19:05:39 +0200

    feat(fix): fix App and deleting ApiProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,23 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ApiProvider } from '@reduxjs/toolkit/query/react';
 import { store } from './features/store';
 import Login from './pages/login';
 import SignUp from './pages/signUp';
 import { ProductsList } from './components/Products/ProductsList';
-import { productsApi } from './features/apiSlice';
 
 export const App: React.FunctionComponent = () => {
 	return (
 		<>
 			<Provider store={store}>
-				<ApiProvider api={productsApi}>
-					<BrowserRouter>
-						<ToastContainer />
-						<Routes>
-							<Route path="/" element={<Login />} />
-							<Route path="/signUp" element={<SignUp />} />
-							<Route path="/products" element={<ProductsList />} />
-						</Routes>
-					</BrowserRouter>
-				</ApiProvider>
+				<BrowserRouter>
+					<ToastContainer />
+					<Routes>
+						<Route path="/" element={<Login />} />
+						<Route path="/signUp" element={<SignUp />} />
+						<Route path="/products" element={<ProductsList />} />
+					</Routes>
+				</BrowserRouter>
 			</Provider>
 			,
 		</>
